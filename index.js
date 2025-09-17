44d87ba54d3f98c237bcd503410f62962664ec47
1094074761@qq.com <1094074761@qq.com>
2025-09-17 09:29:36 +0800

    提交

--- index.js
+++ index.js
@@ -46,84 +46,91 @@ const getCheci = (str) => {
   return result;
 };
 const getDataList = () => {
-  const config = {
-    method: "get",
-    url: `https://kyfw.12306.cn/otn/leftTicket/queryG?leftTicketDTO.train_date=${baseConfig.date}&leftTicketDTO.from_station=${baseConfig.start_station.code}&leftTicketDTO.to_station=${baseConfig.end_station.code}&purpose_codes=ADULT`, // 目标URL
-    headers: {
-    "accept": "*/*",
-    "accept-language": "zh-CN,zh;q=0.9",
-    "cache-control": "no-cache",
-    "if-modified-since": "0",
-    "sec-ch-ua": "\"Chromium\";v=\"140\", \"Not=A?Brand\";v=\"24\", \"Google Chrome\";v=\"140\"",
-    "sec-ch-ua-mobile": "?0",
-    "sec-ch-ua-platform": "\"Windows\"",
-    "sec-fetch-dest": "empty",
-    "sec-fetch-mode": "cors",
-    "sec-fetch-site": "same-origin",
-    "x-requested-with": "XMLHttpRequest"
-  },
-  };
-  // 发送请求
-  axios(config)
-    .then(function (response) {
-      const resData = response.data.data;
-      // console.log(resData.result);
-      const resultList = resData?.result;
-      if (resultList) {
-        const canStart = resultList.some((x) => {
-          let a = x.includes("有");
-          if (pauseList.length > 0) {
-            const checi = getCheci(x);
-            if (pauseList.includes(checi)) {
-              if (!x.includes("暂停发售")) {
-                a = true;
+  console.log(`-------------------------------`)
+  console.log('查询中....')
+  return new Promise((resolve, reject) => {
+    const config = {
+      method: "get",
+      url: `https://kyfw.12306.cn/otn/leftTicket/queryG?leftTicketDTO.train_date=${baseConfig.date}&leftTicketDTO.from_station=${baseConfig.start_station.code}&leftTicketDTO.to_station=${baseConfig.end_station.code}&purpose_codes=ADULT`, // 目标URL
+      headers: {
+        "accept": "*/*",
+        "accept-language": "zh-CN,zh;q=0.9",
+        "cache-control": "no-cache",
+        "if-modified-since": "0",
+        "sec-ch-ua": "\"Chromium\";v=\"140\", \"Not=A?Brand\";v=\"24\", \"Google Chrome\";v=\"140\"",
+        "sec-ch-ua-mobile": "?0",
+        "sec-ch-ua-platform": "\"Windows\"",
+        "sec-fetch-dest": "empty",
+        "sec-fetch-mode": "cors",
+        "sec-fetch-site": "same-origin",
+        "x-requested-with": "XMLHttpRequest"
+      },
+    };
+    // 发送请求
+    axios(config)
+      .then(function (response) {
+        const resData = response.data.data;
+        // console.log(resData.result);
+        const resultList = resData?.result;
+        if (resultList) {
+          const canStart = resultList.some((x) => {
+            let a = x.includes("有");
+            if (pauseList.length > 0) {
+              const checi = getCheci(x);
+              if (pauseList.includes(checi)) {
+                if (!x.includes("暂停发售")) {
+                  a = true;
+                }
               }
             }
-          }
-          return a;
-        });
-        if (initLength === 0) {
-          initLength = resultList.length;
-          resultList.forEach((x) => {
-            if (x.includes("暂停发售")) {
-              pauseList.push(getCheci(x));
-            }
+            return a;
           });
-          console.log('-----------------------')
-          console.log('-----------------------')
-          console.log('-----------------------')
-          console.log(`初始化获取班次总数：${initLength}`);
-          console.log(`初始化获取待发售班次：${pauseList}`);
-          console.log('-----------------------')
-          console.log('-----------------------')
-          console.log('-----------------------')
+          if (initLength === 0) {
+            initLength = resultList.length;
+            resultList.forEach((x) => {
+              if (x.includes("暂停发售")) {
+                pauseList.push(getCheci(x));
+              }
+            });
+            console.log('-----------------------')
+            console.log('-----------------------')
+            console.log('-----------------------')
+            console.log(`初始化获取班次总数：${initLength}`);
+            console.log(`初始化获取待发售班次：${pauseList}`);
+            console.log('-----------------------')
+            console.log('-----------------------')
+            console.log('-----------------------')
+          }
+          
+          console.log(
+            `时间：${formatDate(Date.now())} | 当前班次总数：${resultList.length}`
+          );
+          
+          console.log("是否开售：", canStart ? "是" : "否");
+          if (initLength > 0 && (canStart || resultList.length > initLength)) {
+            wxpusher.sendMessage(`${baseConfig.date}, ${baseConfig.start_station.name} 到 ${baseConfig.end_station.name}新增加班车`);
+          }
         }
-        
-        console.log(
-          `时间：${formatDate(Date.now())} | 当前班次总数：${resultList.length}`
-        );
-        
-        console.log("是否开售：", canStart ? "是" : "否");
-        if (initLength > 0 && (canStart || resultList.length > initLength)) {
-          wxpusher.sendMessage(`${baseConfig.date}, ${baseConfig.start_station.name} 到 ${baseConfig.end_station.name}新增加班车`);
+        clearConsole();
+        resolve()
+      })
+      .catch(function (error) {
+        if (!errWarning) {
+          // errWarning = true
+          // clearInterval(timer)
+          // timer = null
+          wxpusher.sendMessage("12306接口异常");
         }
-      }
-      clearConsole();
-    })
-    .catch(function (error) {
-      if (!errWarning) {
-        // errWarning = true
-        // clearInterval(timer)
-        // timer = null
-        wxpusher.sendMessage("12306接口异常");
-      }
-      console.log(error); // 打印错误信息
-    });
+        console.log(error); // 打印错误信息
+        reject()
+      });
+  })
 };
 
-const loopTask = () => {
-  getDataList();
-  
+const loopTask = async () => {
+  await getDataList();
+  console.log('查询完毕...')
+  console.log('*******************************')
   const interfaceFreq = randomInt(10, 15)
   console.log(`${interfaceFreq}秒后再次检测`)
   timer = setTimeout(() => {
